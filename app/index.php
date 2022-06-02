a455ed69f75cb42b873d77d8f602e9c4d9275aa4
Quentin HESS <quentin.hess@smile.fr>
2022-06-02 15:18:28 +0200

    add logo

--- app/index.php
+++ app/index.php
@@ -95,21 +95,9 @@ $ratio_mandat = round($temps_passe / $total_temps * 100);
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-yellow">
             <div class="container">
-                <a class="navbar-brand" href="#">Plan vélo Montpellier</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="#">Observatoire du vélo Montpellier</a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="https://www.velocite-montpellier.fr/">Site Vélocité</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                  <p class="ms-auto"><a href="https://www.velocite-montpellier.fr/"><img src='assets/logo_VGM.png' /></a></p>
                 </div>
             </div>
         </nav>
